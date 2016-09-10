dd3faf71f6195896c98f8007f60533800961c510
evykassirer <evy.kassirer@gmail.com>
2016-09-10 11:36:45 -0400

    warns when adding inspiration of seemingly wrong type

--- addInspScript.js
+++ addInspScript.js
@@ -12,20 +12,40 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // For overriding submitting the form
-    $("#addContent").submit(addContent);
+    $("#addContent").submit(checkContent);
 });
 
-
-function addContent(event) {
-    StorageArea = chrome.storage.sync;
-
-    // stop form from submitting normally 
+function checkContent(event) {
+    // stop form from submitting normally
     event.preventDefault();
 
     // get values from form
     var $form = $(this),
     content = $form.find('textarea[name="content"]').val(),
     contentType = $form.find('input[name="content-type"]:checked').val();
+
+
+    if (isImage(content) && contentType === 'text') {
+        let r = confirm("You selected text, but that looks like an image url.\n" +
+            "Click OK if you want it to be text input.");
+        if (!r) {
+            return;
+        }
+    } else if (!isImage(content) && contentType === 'image') {
+        let r = confirm("You selected image, but that doesn't look like an image url.\n" +
+            "Click OK if you want it to be an image url.");
+        if (!r) {
+            return;
+        }
+    } else {
+        addContent(content, contentType, $form);
+    }
+
+}
+
+
+function addContent(content, contentType, $form) {
+    StorageArea = chrome.storage.sync;
 
     // store the new content with a new, higher, key
     StorageArea.get("maxKey", (items) => {
@@ -43,6 +63,7 @@ function addContent(event) {
 
     });
 
+
     // add confirmation text above the form
     if (contentType === "image") {
         $("#confirmation").html("Added new image <img src="+content+" />");
@@ -54,3 +75,7 @@ function addContent(event) {
     $form.find('textarea[name="content"]').val('');
 };
 
+function isImage(text) {
+    let re = /^\S+\.(jpg|jpeg|png|gif|bmp)\S*$/i;
+    return !!text.match(re);
+}
